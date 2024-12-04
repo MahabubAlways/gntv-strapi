85744e1d2652ee29cd652ba31a11c29b8094d7dd
Mahabub Hossain <sjl.hsn@gmail.com>
2024-12-04 13:17:12 +0100

    tinyMCE added.

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,4 +6,52 @@ export default () => ({
   seo: {
     enabled: true,
   },
+  tinymce: {
+    enabled: true,
+    config: {
+      editor: {
+        outputFormat: "html",
+        //tinymceSrc: "/tinymce/tinymce.min.js", // USE WITH YOUR PUBLIC PATH TO TINYMCE LIBRARY FOR USING SELF HOSTED TINYMCE
+        editorConfig: {
+          height: 500,
+          menubar: false,
+          extended_valid_elements: "span, img, small",
+          forced_root_block: "",
+          convert_urls: false,
+          entity_encoding: "raw",
+          image_advtab: true,
+          images_upload_url: "http://localhost:4000/upload",
+          plugins:
+            "preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons accordion",
+          toolbar:
+            "undo redo | accordion accordionremove | blocks fontfamily fontsize | bold italic underline strikethrough | align numlist bullist | link image | table media | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | pagebreak anchor codesample | ltr rtl",
+          importcss_append: true,
+          style_formats: [
+            {
+              title: "Headings",
+              items: [
+                { title: "h1", block: "h1" },
+                { title: "h2", block: "h2" },
+                { title: "h3", block: "h3" },
+                { title: "h4", block: "h4" },
+                { title: "h5", block: "h5" },
+                { title: "h6", block: "h6" },
+              ],
+            },
+
+            {
+              title: "Text",
+              items: [
+                { title: "Paragraph", block: "p" },
+                {
+                  title: "Paragraph with small letters",
+                  block: "small",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+  },
 });
